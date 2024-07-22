0095212a2f2bc2440d04d85263eda57c58deefa3
JC (J. Zittnansky) <54894612+jz543fm@users.noreply.github.com>
2024-07-22 21:14:12 +0200

    Update gpg-export.sh

--- gpg-export.sh
+++ gpg-export.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "exports to the file: my-private-key.asc\n"
+echo "exports to the file: ~/.password-store/my-private-key.asc\n"
 gpg --export-secret-keys $ID > ~/.password-store/my-private-key.asc
