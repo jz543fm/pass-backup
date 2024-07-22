b3a7d64fed0e45691a150ef8a4da5fc263d2fb55
JC (J. Zittnansky) <54894612+jz543fm@users.noreply.github.com>
2024-07-22 21:14:54 +0200

    Update pass-restore.sh

--- pass-restore.sh
+++ pass-restore.sh
@@ -3,7 +3,7 @@
 # Prompt the user for the repository URL
 read -p "Enter the repository URL to restore: " repo_url
 git clone $repo_url ~/.password-store/
-echo "Restore GPG key\n"
+echo "Restore GPG key from ~/.password-store/my-private-key.asc\n"
 gpg --import-options restore --import ~/.password-store/my-private-key.asc
 if [ $? -eq 0 ]; then
     echo "The restore of GPG key is completed"
